ac6382621cd7d71f1cad8f80f969307e42b7adad
Levox98 <Levox98@users.noreply.github.com>
2022-06-08 13:37:21 +0500

    Created "Message" classes

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -1,5 +1,31 @@
 package ru.skillbranch.devintensive.models
 
-class User {
+import java.util.*
 
+class User(
+    val id: String,
+    val firstName: String?,
+    val lastName: String?,
+    val avatar: String?,
+    val rating: Int = 0,
+    val respect: Int = 0,
+    val lastVisit: Date? = null,
+    val isOnline: Boolean = false
+    ) {
+
+    constructor(id: String, firstName: String?, lastName: String?): this(
+        id = id,
+        firstName = firstName,
+        lastName = lastName,
+        avatar = null
+    )
+
+    companion object Factory {
+        var lastId = -1
+        fun makeUser(fullName: String): User {
+            lastId++
+            val (firstName, lastName) = fullName.split(" ")
+            return User(id = "$lastId", firstName = firstName, lastName = lastName)
+        }
+    }
 }
